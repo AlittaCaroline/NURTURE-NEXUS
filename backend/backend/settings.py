8869b7b986b3a81c29a8e25283dde56d311004e2
AlittaCaroline <alittacaro@gmail.com>
2025-07-09 20:08:56 +0530

    Use environment variable for SECRET_KEY

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -21,7 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&la7$(0na2&0cmik*8yooh@+7j)mk2!wh5#)t^o$&hzt)z##3w'
+import os
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
